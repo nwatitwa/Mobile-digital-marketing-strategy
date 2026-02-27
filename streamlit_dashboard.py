5a9ec534937353c17fca5a0506b6e94b5ffd1f16
nwatitwa <gracianasaka@gmail.com>
2026-02-27 15:09:55 +0300

    fixing environment

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -10,6 +10,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import warnings
+import altair as alt
 warnings.filterwarnings('ignore')
 
 # Page configuration
